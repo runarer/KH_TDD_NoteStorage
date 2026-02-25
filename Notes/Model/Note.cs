6829696cc2aa32fd4ebcfe05a2f6297d5e921b3c
runarer <95697611+runarer@users.noreply.github.com>
2026-02-25 12:57:42 +0100

    Changes from string to Guid

--- Notes/Model/Note.cs
+++ Notes/Model/Note.cs
@@ -1,11 +1,12 @@
 using System.ComponentModel.DataAnnotations;
+using Notes.Model.RequestResponse;
 
 namespace Notes.Model;
 
 public class Note
 {
     [Key]
-    public required string Id { get; init; }
+    public required Guid Id { get; init; }
     [Required]
     public required string Title { get; set; }
     public string Content { get; set; } = string.Empty;
