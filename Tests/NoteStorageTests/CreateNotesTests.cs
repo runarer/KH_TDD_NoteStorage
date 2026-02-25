05702df9d6ff4952f3ab3c391767c1f493fe21e0
runarer <95697611+runarer@users.noreply.github.com>
2026-02-25 11:00:33 +0100

    Create frame for tests

--- Tests/NoteStorageTests/CreateNotesTests.cs
+++ Tests/NoteStorageTests/CreateNotesTests.cs
@@ -2,5 +2,20 @@
 
 public class CreateNotesTests
 {
+    [Fact]
+    public void CreateNote_CreateANoteWithTitleAndContentOnAnEmptyServer_NoteShouldBeCreatedAndReturnedInBodyAndCode201()
+    {
 
+    }
+    [Fact]
+    public void CreateNote_CreatesANoteWithoutTitle_ShouldNotBeCreatedAndReturn400AndErrorMessage()
+    {
+
+    }
+
+    [Fact]
+    public void CreateNote_CreateNoteWithTitleButNoContent_Return201AndNoteIsCreatedOnServer()
+    {
+
+    }
 }
