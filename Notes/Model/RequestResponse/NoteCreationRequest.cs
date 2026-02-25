2565e0974461013ddc6d4943c3ce91a7a4a27e96
runarer <95697611+runarer@users.noreply.github.com>
2026-02-25 14:04:34 +0100

    Removes required and default value

--- Notes/Model/RequestResponse/NoteCreationRequest.cs
+++ Notes/Model/RequestResponse/NoteCreationRequest.cs
@@ -1,5 +1,4 @@
-using System.ComponentModel.DataAnnotations;
 
 namespace Notes.Model.RequestResponse;
 
-public record NoteCreationRequests([Required] string Title, string Content = "");
+public record NoteCreationRequests(string Title, string Content);
