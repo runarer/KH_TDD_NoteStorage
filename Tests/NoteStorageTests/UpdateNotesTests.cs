ec9171d38dd7d85875d82768ea8bacd1c0163021
runarer <95697611+runarer@users.noreply.github.com>
2026-02-27 10:24:17 +0100

    Created test for update

--- Tests/NoteStorageTests/UpdateNotesTests.cs
+++ Tests/NoteStorageTests/UpdateNotesTests.cs
@@ -19,19 +19,14 @@ public class UpdateNotesTests(WebApplicationFactory<Program> factory) : TestEnvi
     public async Task PUT_CreateANoteThenUpdateItAndReadItBack_Returns200AndTheRightContentInBody()
     {
         var client = Client;
-
-        //Create a note and get its location and content
-        var createdNote = await client.PostAsJsonAsync("/notes", new NoteCreationRequests("Test Note", "Test Content"));
-        createdNote.EnsureSuccessStatusCode();
-        Assert.NotNull(createdNote.Headers.Location);
-        var createdNoteLocation = createdNote.Headers.Location;
-        var createdNoteResponse = await createdNote.Content.ReadFromJsonAsync<NoteResponse>();
-        Assert.NotNull(createdNoteResponse);
+        var createdNote = await AddNoteToServer(client, new NoteCreationRequests("Test Note", "Test Content"));
+        var createdNoteLocation = await GetLocationOfResponse(createdNote);
+        var createdNoteResponse = await GetNoteResponse(createdNote);
 
         var updatedTitle = "Updated test note";
         var updatedContent = "Updated note content";
 
-        //Action: update the note
+        // Action: update the note
         var updatedNote = new NoteUpdateRequest(createdNoteResponse.Id, updatedTitle, updatedContent);
 
         // Assert
@@ -54,34 +49,85 @@ public class UpdateNotesTests(WebApplicationFactory<Program> factory) : TestEnvi
     {
         var client = Client;
         var createdNote = await AddNoteToServer(client, new NoteCreationRequests("Test Note", "Test Content"));
+        var createdNoteLocation = await GetLocationOfResponse(createdNote);
+        var createdNoteResponse = await GetNoteResponse(createdNote);
+        var wrongGuid = Guid.NewGuid();
+        Assert.NotEqual(createdNoteResponse.Id, wrongGuid);
         var updatedNote = new NoteUpdateRequest(Guid.NewGuid(), "updatedTitle", "updatedContent");
 
         //Action: update the note
-        var response = await client.PutAsJsonAsync(createdNote, updatedNote);
+        var response = await client.PutAsJsonAsync(createdNoteLocation, updatedNote);
 
         // Assert
         Assert.Equal(HttpStatusCode.UnprocessableEntity, response.StatusCode);
-        var badRequest = await response.Content.ReadAsStringAsync();
-        Assert.Contains("Id in body did not match id in link.", badRequest);
+        var unprocessableEntity = await response.Content.ReadAsStringAsync();
+        Assert.Contains("Id in body did not match id in link.", unprocessableEntity);
 
     }
 
-    [Fact(Skip = "Not implemented yet")]
+    [Fact]
     public async Task PUT_UpdateANoteThatDoesntExsistServerEmpty_Returns404AndErrorMessage()
     {
+        var client = Client;
+        var noteGuid = Guid.NewGuid();
+        var updatedNote = new NoteUpdateRequest(noteGuid, "updatedTitle", "updatedContent");
 
+        var response = await client.PutAsJsonAsync($"/notes/{noteGuid}", updatedNote);
+
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        var notFound = await response.Content.ReadAsStringAsync();
+        Assert.Contains("Note not found!", notFound);
     }
 
-    [Fact(Skip = "Not implemented yet")]
+    [Fact]
     public async Task PUT_CreateSeveralNotesThenUpdateOneAndReadItBack_Returns200AndTheRightContentInBody()
     {
+        var client = Client;
 
+        var createdNotes = await FillServerWithNotes(client, notes);
+        var locationOfThirdNote = await GetLocationOfResponse(createdNotes[2]);
+        var thirdNoteResponse = await GetNoteResponse(createdNotes[2]);
+        var updatedTitle = "Updated Title";
+        var updatedContent = "Updated Content";
+        var updatedNote = new NoteUpdateRequest(thirdNoteResponse.Id, updatedTitle, updatedContent);
+
+        // Action
+        var responseUpdateNote = await client.PutAsJsonAsync(locationOfThirdNote, updatedNote);
+        responseUpdateNote.EnsureSuccessStatusCode();
+        var responseReadUpdatedNote = await client.GetAsync(locationOfThirdNote);
+        responseReadUpdatedNote.EnsureSuccessStatusCode();
+
+        // Assert
+        var noteResponse = await responseReadUpdatedNote.Content.ReadFromJsonAsync<NoteResponse>();
+
+        Assert.NotNull(noteResponse);
+        Assert.Equal(updatedNote.Id, noteResponse.Id);
+        Assert.Equal(updatedTitle, noteResponse.Title);
+        Assert.Equal(updatedContent, noteResponse.Content);
     }
 
-
-    [Fact(Skip = "Not implemented yet")]
+    [Fact]
     public async Task PUT_CreateSeveralNotesThenUpdateOneThatDoesntExsist_Returns404AndErrorMessage()
     {
+        var client = Client;
 
+        // Fill server with notes and make a wrong GUID,
+        var createdNotes = await FillServerWithNotes(client, notes);
+        var wrongGuid = Guid.NewGuid();
+        var updatedNote = new NoteUpdateRequest(wrongGuid, "Title of wrong update", "Content of wrong update");
+
+        // make sure the notes on the server does not have the same GUID
+        var createdNotesResponsesTask = createdNotes.Select(GetNoteResponse);
+        var createdNotesResponses = await Task.WhenAll(createdNotesResponsesTask);
+        var createdNotesGuids = createdNotesResponses.Select(item => item.Id).ToArray();
+        Assert.DoesNotContain(wrongGuid, createdNotesGuids);
+
+        // Action
+        var response = await client.PutAsJsonAsync($"/notes/{wrongGuid}", updatedNote);
+
+        // Assert
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        var notFound = await response.Content.ReadAsStringAsync();
+        Assert.Contains("Note not found!", notFound);
     }
 }
